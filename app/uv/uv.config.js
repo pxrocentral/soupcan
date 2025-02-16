197e32ba83bc3a60c0aa02681717bd40adc6388e
Pyro Central <sudo.user.tlopez@gmail.com>
2025-02-16 15:45:14 +0000

    Update uv.config.js

--- app/uv/uv.config.js
+++ app/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/soup/can/',
+    prefix: '/app/soup-can/',
     bare: "https://sysadmin.us.kg/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
